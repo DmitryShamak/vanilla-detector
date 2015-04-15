75513a44ca1e169d9c04468ea50d143c9e0869cd
DmitryShamak <hellkitedmitry@mail.ru>
2015-04-15 16:36:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,84 +21,105 @@ var getSettledData = function(data) {
 	return data;
 };
 
-var getProfileFriends = function(id, resolver) {
+var getProfileFriends = function(id) {
 	var url = "https://api.vk.com/method/friends.get?user_id=" + id + "&v=5.29";
+
+	var resolver = Promise.pending();
 
 	request(url, function (err, response, body) {
 		if (err) {
 			return resolver.reject(err);
 		}
 
-
 		var items = JSON.parse(body);
 
-		//return only 5 friends for easy test
-		var friends = items.response.items.slice(0, 6);
+		//return only 3 friends for easy test
+		if(items.error) {
+			return resolver.reject(items.error);
+		}
+
+		var max = 3;
+		var friends = items.response.items.slice(0, max);
 
 		resolver.resolve(friends);
 	});
-};
-
-var crawlFriends = function(friends, level, resolver) {
-	var resolver = Promise.pending();
-
-	console.log("start crawl", friends, level);
-
-	if(level > 0) {
-		var promises = friends.map(function(id) {
-			var innerResolver = Promise.pending();
-			
-			getProfileFriends(id, innerResolver);
-
-			return innerResolver.promise;
-		});
-
-		Promise.settle(promises).then(function(res) {
-			resolver.resolve(res);
-			//need to go deeper --level
-		});
-	}
 
 	return resolver.promise;
 };
 
-var getProfileFollowers = function(id, author, resolver) {
+var crawlFriends = function(friends, level, results, resolver) {
+	var resolver = (resolver) ? resolver : Promise.pending();
+
+	results = (results) ? results : [];
+
+	var promises = friends.map(function(id) { //timeout need to replace with concurency in future
+		return setTimeout(function() {
+			return getProfileFriends(id);
+		}, 1000); 
+	});
+
+	Promise.settle(promises).then(function(res) {
+		res = getSettledData(res);
+
+		results = results.concat(res);
+		if(level > 0) {
+			crawlFriends(res, --level, results, resolver);
+		} else {
+			resolver.resolve(results);
+		}
+	}, function(rej) {
+		console.log("Reject", rej);
+	});
+
+	return resolver.promise;
+};
+
+var getProfileFollowers = function(id, author) {
 	var url = "https://api.vk.com/method/users.getFollowers?user_id=" + id + "&v=5.29";
+
+	var resolver = Promise.pending();
 
 	request(url, function (err, response, body) { //get followers -> change to get audio in future
 		if (err) {
 			return resolver.reject(err);
 		}
 		
-		body = JSON.parse(body).response;
-		var result = {id: id, count: body.count};
+		items = JSON.parse(body).response;
+
+		if(items.error) {
+			console.log("Error followers", items.error);
+			return resolver.reject(items.error);
+		}
+
+		var result = {id: id, followersCount: items.count};
 
 		resolver.resolve(result);
 	});
+
+	return resolver.promise;
 };
 
 var findAudios = function(userList, author) {
 	var resolver = Promise.pending();
 
-	userList = getSettledData(userList);
-
-	var promises = userList.map(function(id) {
-		var innerResolver = Promise.pending();
-		
-		getProfileFollowers(id, author, innerResolver);
-
-		return innerResolver.promise;
+	var promises = userList.map(function(id) {	//timeout need to replace with concurency in future
+		return setTimeout(function() {
+			return getProfileFollowers(id, author);
+		}, 1000);
 	});
 
 	Promise.settle(promises).then(function(res) {
+		res = getSettledData(res);
 		resolver.resolve(res);
+	}, function(rej) {
+		console.log("Reject", rej);
 	});	
 
 	return resolver.promise;
 };
 
 var compare = function(a,b) {
-	var key = "count";
+	var key = "followersCount";
 
 	if (a[key] < b[key]) {
 		return -1;
@@ -114,14 +135,14 @@ var getTopList = function(list, limit) {
 	return list.sort(compare).reverse().slice(0, ++limit);
 };
 
-var startDetecting = function(id, author, level, callback) { //userId, media author, how deep we will search by friends, callback after all done  
-	crawlFriends([id], level).then(function(res) {
+var startDetecting = function(id, author, level) { //userId, media author, how deep we will search by friends, callback after all done  
+	return crawlFriends([id], level).then(function(res) {
 		return findAudios(res, author);
-	}).then(function(res) {
-		res = getSettledData(res);
-
-		callback(null, getTopList(res, 3));
+	}, function(rej) {
+		console.log("Reject", rej);
 	});
 };
 
 module.exports.startDetecting = startDetecting;
+
+module.exports.getTopList = getTopList;
